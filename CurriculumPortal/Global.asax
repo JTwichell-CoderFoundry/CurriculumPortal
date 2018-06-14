dd25d7f083f9b77809a86b724d1ebe142ee05a33
Jason Twichell <jtwichell@core-techs.net>
2018-06-14 14:23:20 -0400

    Initial project commit

---
+++ CurriculumPortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CurriculumPortal.MvcApplication" Language="C#" %>
---
+++ CurriculumPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CurriculumPortal.Startup))]
+namespace CurriculumPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
